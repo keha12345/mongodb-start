4cdb46be673a7a7c82e4125e56a3f6c8ff618497
keha12345 <keha12345@gmail.com>
2023-11-03 06:12:32 +0200

    init

---
+++ mongoInstall.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt install -y software-properties-common gnupg apt-transport-https ca-certificates
+sudo apt install -y mongodb
+wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
+sudo apt update
+sudo apt install -y mongodb-org
